52b97117f1dd6418428578fdaccee681851b9a48
Cory Douthat <soldierforchrist2@gmail.com>
2026-01-15 01:27:01 -0500

    Changes to compile with non-hpp math libraries. May change back or abandon later

--- src/phVkEngine.hpp
+++ src/phVkEngine.hpp
@@ -22,9 +22,9 @@
 #include <imgui_impl_sdl3.h>
 #include <imgui_impl_vulkan.h>
 
-#include "Vec.hpp"
-#include "Quat.hpp"
-#include "Mat.hpp"
+#include "vec.h"
+#include "quat.h"
+#include "mat.h"
 #include "array_list.hpp"
 
 #include "phVkTypes.hpp"
